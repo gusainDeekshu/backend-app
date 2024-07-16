fdd969db8304b560c566d3213ea73fce884caf1a
Deekshant <153889930+gusainDeekshu@users.noreply.github.com>
2024-07-16 10:47:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+
 const express=require('express');
 const cors=require('cors');
 const newLocal = `./db`;
 const { connectMongodb } = require(newLocal);
 const newLocal_1 = './routes/foodroutes';
+const newLocal_2 = './routes/userroute';
 const foodrouter=require(newLocal_1)
+const userrouter=require(newLocal_2)
 
 
 // app config
@@ -15,6 +18,8 @@ const port=4000;
 app.use(express.json());
 app.use(cors());
 
+app.get('/',(req,res)=>{res.send("api working")});
+
 //establishin db conex=ctions
 connectMongodb('mongodb://127.0.0.1:27017/foodapp')
 
@@ -24,6 +29,9 @@ app.use("/images",express.static("uploads"));
 app.get("/",(req,res)=>{
     res.send("working api")
 })
+//api endpoint for user
+app.use("/api/user",userrouter);
+
 
 
 
